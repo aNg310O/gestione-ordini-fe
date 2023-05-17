0694d0f2ea1ab4108b7d2a271da190df509a9ac7
aNg310O <56519238+aNg310O@users.noreply.github.com>
2023-05-17 11:49:43 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   //baseURL: `http://localhost:8082/api/gestione-ordini/`,
   //baseURL: `http://localhost:8081/`,
-  baseURL: `https://c1-gestione-ordini.herokuapp.com/`,
+  baseURL: `https://gestione-ordini-be.vercel.app/`,
   responseType: "json"
 });
